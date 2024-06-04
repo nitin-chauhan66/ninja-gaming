b3aba3575a142e00fbd9e8d139755fcb44242bc0
Nitin Chauhan <nvch66@gmail.com>
2024-06-04 21:44:03 +0530

    Ninja Gaming

---
+++ src/routes/+page.svelte
@@ -0,0 +1,15 @@
+<div class="index">
+    <h2>Welcome</h2>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse nobis ea ad ab impedit hic, vel molestias. Suscipit magnam, totam eius eligendi non neque cumque unde rem iure quas ad?</p>
+
+   <a href="/guide">View guide</a>
+   <a href="/about">About</a>
+</div>
+
+<style>
+    .index {
+        text-align: center;
+        display: block;
+        margin: 20px auto;
+    }
+</style>
---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,15 @@
+<div class="about">
+    <h2>Welcome</h2>
+    <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi repudiandae facere repellendus unde beatae accusamus nulla consequuntur quaerat sapiente accusantium. Quae facere rem quidem. Repellendus nulla dicta consequuntur iusto tempore.
+    </p>
+    <a href="/">Home</a>
+</div>
+
+<style>
+    .about {
+        text-align: center;
+        display: block;
+        margin: 20px auto;
+    }
+</style>
